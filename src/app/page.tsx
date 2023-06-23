059ca0181f4acc3e71f48779043d29ef25e56c84
Joe Reeve <joe.reeve@amplitude.com>
2023-06-23 22:33:23 +0100

    INIT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,75 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const [status, setStatus] = useState<"complete" | "loading">("complete");
+  const [tweet, setTweet] = useState<string>("");
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <p>Tweetify. Add your notes, and hit the big button...</p>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <form
+          className={styles.form}
+          onSubmit={async (ev) => {
+            ev.preventDefault();
+            try {
+              setStatus("loading");
+              const form = ev.target as HTMLFormElement;
+              const url = form.url.value;
+              const context = form.context.value;
+
+              const { tweet } = await fetch(`http://localhost:4000/tweetify`, {
+                method: "POST",
+                body: JSON.stringify({ urls: [url], context }),
+                headers: { "Content-Type": "application/json" },
+              }).then((res) => res.json());
+
+              console.log(tweet);
+              setTweet(tweet);
+            } finally {
+              setStatus("complete");
+            }
+          }}
+        >
+          <input
+            type="url"
+            name="url"
+            id="url"
+            placeholder="URL (optional)"
+            className={styles.url}
+          />
+          <br />
+
+          <textarea
+            className={styles.context}
+            name="context"
+            id="context"
+            placeholder="Your context/opinions"
+          />
+          <br />
+
+          <button
+            type="submit"
+            className={styles.submit}
+            disabled={status === "loading"}
+          >
+            TURN THE ABOVE INTO A TWEET
+          </button>
+          <br />
+          <br />
+          <br />
+          {tweet && <div className={styles.tweet}>{tweet}</div>}
+        </form>
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+       
       </div>
     </main>
-  )
+  );
 }
